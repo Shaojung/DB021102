c68e9d57de7419ab6a96724e896e3d7b21076882
shaojung <shaojung@gmail.com>
2017-02-11 15:42:30 +0800

    use GSon to parse data.

--- app/src/main/java/com/example/student/db021102/MainActivity.java
+++ app/src/main/java/com/example/student/db021102/MainActivity.java
@@ -9,6 +9,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,17 +27,24 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         Log.d("NET", response);
-                        try {
-                            JSONArray array = new JSONArray(response);
-                            int i;
-                            for (i=0;i<array.length();i++)
-                            {
-                                JSONObject obj = array.getJSONObject(i);
-                                Log.d("NET", obj.getString("district"));
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                        Gson gson = new Gson();
+                        Animal[] array = gson.fromJson(response, Animal[].class);
+
+                        for (Animal a : array)
+                        {
+                            Log.d("NET", a.district);
                         }
+//                        try {
+//                            JSONArray array = new JSONArray(response);
+//                            int i;
+//                            for (i=0;i<array.length();i++)
+//                            {
+//                                JSONObject obj = array.getJSONObject(i);
+//                                Log.d("NET", obj.getString("district"));
+//                            }
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
 
                     }
                 }, new Response.ErrorListener() {
