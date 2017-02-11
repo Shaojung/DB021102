244a6f0071f96a8d1dd95df910a817dc0db5d1a0
shaojung <shaojung@gmail.com>
2017-02-11 15:34:12 +0800

    use Volley to get open-data

---
+++ app/src/main/java/com/example/student/db021102/Animal.java
@@ -0,0 +1,12 @@
+package com.example.student.db021102;
+
+/**
+ * Created by student on 2017/2/11.
+ */
+
+public class Animal {
+    String district;
+    String address;
+    String tel;
+    String opening_hours;
+}
